12324017481bc23119373c0c68e71e3b133597f1
r0nuh <cs.szilard@gmail.com>
2017-11-27 17:24:28 +0100

    feat(day1): add exercise5

---
+++ week-02/day-1/excercise5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints a few operations on two numbers: 22 and 13
+
+            int number1;
+            int number2;
+
+            number1 = 13;
+            number2 = 22;
+
+            // Print the result of 13 added to 22
+            Console.WriteLine(value: number2 + number1);
+
+            // Print the result of 13 substracted from 22
+            Console.WriteLine(value: number2 - number1);
+
+            // Print the result of 22 mutiplied by 13
+            Console.WriteLine(value: number2 * number1);
+
+
+            // Print the result of 22 divided by 13 (as a decimal fraction)
+          
+            Console.WriteLine(value: number2 / (double)number1);
+
+            // Print the integer part of 22 divided by 13
+            Console.WriteLine(value: number2 / number1);
+
+            // Print the reminder of 22 divided by 13
+            Console.WriteLine(value: number2 % number1);
+
+            Console.ReadLine();
+        }
+    }
+}
