1692ce4081bb9ef461b6f38b217bc093a0e6a913
r0nuh <cs.szilard@gmail.com>
2017-11-27 18:21:00 +0100

    feat(day1): add exercise6

---
+++ week-02/day-1/exercise6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // An average Green Fox attendee codes 6 hours daily
+            // The semester is 17 weeks long
+
+            int hours = 6;
+            int weeks = 17;
+            
+            //
+            // Print how many hours is spent with coding in a semester by an attendee,
+            // if the attendee only codes on workdays.
+            Console.WriteLine(value: 5 * hours * weeks);
+            //
+            // Print the percentage of the coding hours in the semester if the average
+            // work hours weekly is 52
+            double percentage;
+            percentage = (hours * 5) / 52.0 * 100;
+            Console.WriteLine(percentage + "%");
+
+            Console.ReadLine();
+
+        }
+    }
+}
