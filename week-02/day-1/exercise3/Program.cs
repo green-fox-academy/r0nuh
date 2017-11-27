684e398e7fbfd53b5a6b307a15d347920f8f3a58
r0nuh <cs.szilard@gmail.com>
2017-11-27 16:24:25 +0100

    feat(day1): add exercise3

---
+++ week-02/day-1/exercise3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Greet 3 of your classmates with this program, in three separate lines
+            // like:
+            //
+            // Hello, Esther!
+            // Hello, Mary!
+            // Hello, Joe!
+            Console.WriteLine("Hello Szabolcs!");
+            Console.WriteLine("Hello Misi!");
+            Console.WriteLine("Hello Marci!");
+            Console.ReadLine();
+        }
+    }
+}
