74b78647709ee62486e8ae62e2006897c118b6c0
r0nuh <cs.szilard@gmail.com>
2017-11-28 13:34:48 +0100

    feat(day1): add exercise24

---
+++ week-02/day-1/exercise24/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints all the even numbers between 0 and 500
+
+            for (int i = 0; i <= 500; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
