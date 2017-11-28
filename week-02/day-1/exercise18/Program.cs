5a9b39d0f521b2ca4050d78637d3da0901c2f2db
r0nuh <cs.szilard@gmail.com>
2017-11-28 12:16:50 +0100

    feat(day1): add exercise14 & exercise18

---
+++ week-02/day-1/exercise18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number form the standard input,
+            // Than prints "Odd" if the number is odd, or "Even" it it is even.
+            Console.Write("Write a number between 0 and 1000: ");
+            int number = Convert.ToInt16(Console.ReadLine());
+            int result = number % 2;
+
+            if (result == 0)
+            {
+                Console.WriteLine("It is Even");
+            }
+            else
+            {
+                Console.WriteLine("It is Odd");
+            }
+            Console.Read();
+        }
+    }
+}
