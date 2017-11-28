65bbd3d16a49daf04185fed2749501eb597d66c6
r0nuh <cs.szilard@gmail.com>
2017-11-28 16:36:59 +0100

    feat(week2): add day2 exercise2

---
+++ week-02/day-2/exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `al` and assign the value `Greenfox` to it
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+
+            string al = "Greenfox";
+
+            Greet(al);
+            Console.ReadLine();
+        }
+
+        static void Greet(string al)
+        {
+            Console.WriteLine("Hi there " + al + "!");
+        }
+    }
+}
