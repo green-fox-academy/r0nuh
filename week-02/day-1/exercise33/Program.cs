1d96b4a38035f134dc5cdaa5b45a503ce3a7fb10
r0nuh <cs.szilard@gmail.com>
2017-11-28 23:40:15 +0100

    feat(week2): add day1 exercise33

---
+++ week-02/day-1/exercise33/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores a number, and the user has to figure it out.
+            // The user can input guesses, after each guess the program would tell one
+            // of the following:
+            //
+            // The stored number is higher
+            // The stried number is lower
+            // You found the number: 8
+
+            int x = 33;
+            int guess = 0;
+
+            while (guess != x)
+            {
+                Console.Write("Guess the number between 0 and 1000: ");
+                guess = Convert.ToInt16(Console.ReadLine());
+
+                if (guess > x)
+                {
+                    Console.WriteLine("The stored number is lower");
+                }
+                else if (guess < x)
+                {
+                    Console.WriteLine("The stored number is higher");
+                }
+            }
+
+            Console.WriteLine("You found the number: {0}", x);            
+            Console.Read();
+            
+        }
+    }
+}
