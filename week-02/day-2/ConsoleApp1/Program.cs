5599a6c28ac931c265f9a8a1829fa555e3c1177d
r0nuh <cs.szilard@gmail.com>
2017-11-28 16:48:40 +0100

    feat(week2): add day2 exercise3

---
+++ week-02/day-2/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `am` and assign the value `kuty` to it
+            // - Write a function called `appendA` that gets a string as an input
+            //   and appends an 'a' character to its end
+            // - Print the result of `appendA(am)`
+
+            string am = "kuty";
+
+            AppendA(am);
+            Console.ReadLine();
+        }
+        static void AppendA(string am)
+        {
+            Console.WriteLine(am + "a");
+        }
+    }
+}
