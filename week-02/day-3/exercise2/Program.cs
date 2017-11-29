42bb7c614372bc2a1545973935d29c5de4ec2dd6
r0nuh <cs.szilard@gmail.com>
2017-11-29 14:17:25 +0100

    feat(week2): add day3 exercise2

---
+++ week-02/day-3/exercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UrlFixer
+{
+    public class UrlFixer
+    {
+        public static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+
+            // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+            // Also, the URL is missing a crutial component, find out what it is and insert it too!
+            // Try to solve it more than once using different string functions!
+
+            url = url.Replace("bots", "odds").Insert(5, ":");
+
+            Console.WriteLine(url);
+            Console.Read();
+        }
+    }
+}
