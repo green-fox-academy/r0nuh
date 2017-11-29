7d001417f517cb8e88843666214cae8a4da55c33
r0nuh <cs.szilard@gmail.com>
2017-11-29 13:30:02 +0100

    feat(week2): add day2 exercise4

---
+++ week-02/day-2/exercise4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+            int x = 6;
+
+            Sum(x);
+            Console.ReadLine();
+        }
+
+        static void Sum(int x)
+        {
+            int j = 0;
+            for(int i = 0; i < x; i++)
+            {
+                j += i;
+            }
+            Console.WriteLine(x + j);
+        }
+    }
+}
