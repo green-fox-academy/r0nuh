2bebe1f52d77124cc578401c1162775025b4a814
r0nuh <cs.szilard@gmail.com>
2017-11-29 16:25:16 +0100

    feat(week2): add day3 exercise3

---
+++ week-02/day-3/exercise3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace TakesLonger
+{
+    public class TakesLonger
+    {
+        public static void Main(string[] args)
+        {
+            string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+            // When saving this quote a disk error has occured. Please fix it.
+            // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+            // Using pieces of the quote variable (instead of just redefining the string)
+
+            StringBuilder fix = new StringBuilder();
+            
+            fix.Append("Hofstadter's Law: It ");
+            fix.Append("always takes longer than ");
+            fix.Append("you expect, even when you take into account Hofstadter's Law.");
+
+            Console.WriteLine(fix);
+            Console.Read();
+        }
+    }
+}
