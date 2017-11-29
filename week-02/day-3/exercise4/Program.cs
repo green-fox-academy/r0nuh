1e772ca25cca41396c6df0d22d001547b111e59e
r0nuh <cs.szilard@gmail.com>
2017-11-29 17:21:29 +0100

    feat(week2): add day3 exercise4

---
+++ week-02/day-3/exercise4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace TodoPrint
+{
+    public class TodoPrint
+    {
+        public static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indention
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+            var todos = new StringBuilder(todoText);
+            
+            todos.Insert(0, "My todo:\n").Append(" - Download games\n     - Diablo");
+
+            Console.WriteLine(todos);
+            Console.Read();
+        }
+    }
+}
