469ec929646745b23c7a893c83b3ca068b48882a
r0nuh <cs.szilard@gmail.com>
2017-11-29 23:17:17 +0100

    feat(week2): add day1 exercise10

---
+++ week-02/day-1/exercise10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define several things as a variable then print their values
+            // Your name as a string
+            // Your age as an integer
+            // Your height in meters as a double
+            // Whether you are married or not as a boolean
+
+            string name = "Szilard";
+            int age = 34;
+            double height = 1.72;
+            bool married = false;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(married);
+            Console.Read();
+        }
+    }
+}
