fe71b771e5ed11ca1ff0d13fa5fd4a31518cf7e3
r0nuh <cs.szilard@gmail.com>
2017-11-30 16:50:34 +0100

    feat(week2): add day1 exercise20

---
+++ week-02/day-1/exercise20/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+
+            Console.WriteLine("Write two numbers: ");
+            int number0 = Convert.ToInt32(Console.ReadLine());
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            if (number0 > number1)
+            {
+                Console.WriteLine(number0 + " is the bigger one");
+            }
+            else if (number0 < number1)
+            {
+                Console.WriteLine(number1 + " is the bigger one");
+            }
+            else
+            {
+                Console.WriteLine("They are equal");
+            }
+
+            Console.Read();
+        }
+    }
+}
