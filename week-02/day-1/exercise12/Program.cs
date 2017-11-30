0ede9fd5a795911056d0cd87591580525c48418d
r0nuh <cs.szilard@gmail.com>
2017-11-30 10:17:38 +0100

    feat(week2): add day1 exercise12

---
+++ week-02/day-1/exercise12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores 3 sides of a cuboid as variables (doubles)
+            // The program should write the surface area and volume of the cuboid like:
+            //
+            // Surface Area: 600
+            // Volume: 1000
+
+            int a = 1;
+            int b = 2;
+            int c = 3;
+
+            int volume = a * b * c;
+            int area = 2 * (a * b + a * c + b * c);
+
+            Console.WriteLine("Surface Area: " + area);
+            Console.WriteLine("Volume: " + volume);
+            Console.Read();
+        }
+    }
+}
