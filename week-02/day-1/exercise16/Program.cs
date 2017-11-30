5bd1138fc451295dd87f6467d42c990c1ffac06a
r0nuh <cs.szilard@gmail.com>
2017-11-30 11:58:39 +0100

    feat(week2): add day1 exercise16

---
+++ week-02/day-1/exercise16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The seconf represents the number of pigs the farmer has
+            // It should print how many legs all the animals have
+
+            Console.Write("chikens: ");
+            int chickens = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("pigs: ");
+            int pigs = Convert.ToInt32(Console.ReadLine());
+
+            int legs = chickens * 2 + pigs * 4;
+
+            Console.WriteLine("number of legs: " + legs);
+            Console.Read();
+        }
+    }
+}
--- week-02/day-1/exercise16/Properties/AssemblyInfo.cs
+++ week-02/day-1/exercise16/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("exercise14")]
+[assembly: AssemblyTitle("exercise16")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("exercise14")]
+[assembly: AssemblyProduct("exercise16")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("315d4f3f-10e7-470a-a482-6e9d99988d0f")]
+[assembly: Guid("942eeab1-f6d9-4932-a758-9205416c5e00")]
 
 // Version information for an assembly consists of the following four values:
 //
