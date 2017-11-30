ad72be098a67a15bfbdf1d434077183e3e709aec
r0nuh <cs.szilard@gmail.com>
2017-11-30 10:58:22 +0100

    feat(week2): add day1 exercise15

---
+++ week-02/day-1/exercise15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+
+            //Console.Write("km: ");
+            int km = Convert.ToInt32(Console.Read());
+
+            Console.WriteLine(km * 0.621371);
+            Console.ReadLine();
+        }
+    }
+}
