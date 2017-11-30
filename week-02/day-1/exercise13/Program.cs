c7ca40443cc01cdaafab4e195fa6f350ab370ea8
r0nuh <cs.szilard@gmail.com>
2017-11-30 10:30:18 +0100

    feat(week2): add day1 exercise13

---
+++ week-02/day-1/exercise13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented bt the variables
+
+            int dayInSeconds = 60 * 60 * 24;
+            int currentInSeconds = (currentHours * 60 * 60) + (currentMinutes * 60) + currentSeconds;
+
+            Console.WriteLine(dayInSeconds - currentInSeconds);
+            Console.Read();
+        }
+    }
+}
