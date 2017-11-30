9e0c1363cb05fbac3102892cccf1f8d321fee7ee
r0nuh <cs.szilard@gmail.com>
2017-11-30 16:16:09 +0100

    feat(week2): add day1 exercise17 and exercise19

---
+++ week-02/day-1/exercise17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for 5 integers in a row,
+            // then it should print the sum and the average of these numbers like:
+            //
+            // Sum: 22, Average: 4.4
+
+            //string[] input = 
+            int number0 = Convert.ToInt32(Console.ReadLine());
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            int number4 = Convert.ToInt32(Console.ReadLine());
+
+
+            int sum = number0 + number1 + number2 + number3 + number4;
+            int avg = sum / 5;
+
+            Console.WriteLine("Sum: " + sum + "," + "Average: " + avg);
+            Console.Read();
+        }
+    }
+}
---
+++ week-02/day-1/exercise19/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number form the standard input,
+            // If the number is zero or smaller it should print: Not enough
+            // If the number is one it should print: One
+            // If the number is two it should print: Two
+            // If the number is more than two it should print: A lot
+
+            Console.Write("Write a number between -1000 and 1000: ");
+            int number = Convert.ToInt16(Console.ReadLine());
+
+            if (number <= 0)
+            {
+                Console.WriteLine("Not enough");
+            }
+            else if (number == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            else if (number > 2)
+            {
+                Console.WriteLine("A lot");
+            }
+
+            Console.Read();
+        }
+    }
+}
