32fb3b41a18193114d929cd9b2d6d9e98d806941
r0nuh <cs.szilard@gmail.com>
2017-12-01 10:33:16 +0100

    feat(week2): add day5 anagram reverse inputs

---
+++ week-02/day-5/anagram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Give two words:");
+            string input1 = Console.ReadLine();
+            string reverse1 = new string(input1.ToCharArray().Reverse().ToArray());
+
+            string input2 = Console.ReadLine();
+            string reverse2 = new string(input2.ToCharArray().Reverse().ToArray());
+
+            //char[] input1array = input1.ToCharArray();
+            Console.WriteLine(input2);
+            Console.Read();
+        }
+    }
+}
