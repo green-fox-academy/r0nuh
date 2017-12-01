ce71c9c5b48651f966d48eeac0b465ea0d8b117f
r0nuh <cs.szilard@gmail.com>
2017-12-01 12:36:31 +0100

    feat(week2): add day5 palindrome

---
+++ week-02/day-5/palindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreatePalindrome();
+
+        }
+        static void CreatePalindrome()
+        {
+            Console.WriteLine("Give a word:");
+            string input = Console.ReadLine();
+
+            string reverse = new string(input.ToCharArray().Reverse().ToArray());
+
+            Console.WriteLine(input + reverse);
+            Console.Read();
+        }
+    }
+}
