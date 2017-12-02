e12e68cdd9d1fb2112daa8e59d7793e2772faf37
r0nuh <cs.szilard@gmail.com>
2017-12-02 14:23:59 +0100

    feat(week2): day5 anagram works with every lower case word

--- week-02/day-5/anagram/Program.cs
+++ week-02/day-5/anagram/Program.cs
@@ -7,18 +7,31 @@ namespace anagram
     {
         static void Main(string[] args)
         {
-            Anagram();
+            IsAnagram();
 
         }
-        static void Anagram()
+        static void IsAnagram()
         {
             Console.WriteLine("Give two words:");
             string input1 = Console.ReadLine();
             string input2 = Console.ReadLine();
 
-            string reverse2 = new string(input2.ToCharArray().Reverse().ToArray());
+            if (input1.Length == input2.Length)
+            {
+                char[] elementsOfInput1 = input1.ToCharArray();
+                Array.Sort(elementsOfInput1);
+                string sortedInput1 = new string(elementsOfInput1);
 
-            Console.WriteLine(String.Equals(input1, reverse2));
+                char[] elementsOfInput2 = input2.ToCharArray();
+                Array.Sort(elementsOfInput2);
+                string sortedInput2 = new string(elementsOfInput2);
+                
+                Console.WriteLine("Are these two words anagrams? " + String.Equals(sortedInput1, sortedInput2));
+            }
+            else
+            {
+            Console.WriteLine("Are these two words anagrams? " + String.Equals(input1, input2));
+            }
             Console.Read();
 
         }
