b9d3feccb7899b83f2a9f66efce461d433b2f613
r0nuh <cs.szilard@gmail.com>
2017-12-02 23:13:12 +0100

    feat(week2): add day2 array exercise7

---
+++ week-02/day-2/arrayExercise7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DiagonalMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create (dynamically) a two dimensional array
+            //   with the following matrix. Use a loop!
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            // - Print this two dimensional array to the output
+
+            Console.Write("Give the parameter of the equal size matrix: ");
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++) 
+                {
+                    if (x==y) 
+                    {
+                        Console.Write("1 ");
+                    }
+                    else 
+                    {
+                        Console.Write("0 ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
