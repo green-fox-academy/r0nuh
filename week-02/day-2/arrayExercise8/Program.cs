3e78f766f2bcb729bbebb1f0c716ce2b1e957d27
r0nuh <cs.szilard@gmail.com>
2017-12-02 23:20:46 +0100

    feat(week2): add day2 array exercise8

---
+++ week-02/day-2/arrayExercise8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `ag`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Double all the values in the array
+
+            int[] ag = { 3, 4, 5, 6, 7 };
+
+            foreach (int number in ag)
+            {
+                Console.Write(number * 2 + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
