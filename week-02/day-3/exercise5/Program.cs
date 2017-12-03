bce4ceee2b5aabfc49ca3319a19139d4fd9cb042
r0nuh <cs.szilard@gmail.com>
2017-12-03 15:47:05 +0100

    feat(week2): add day3 string exercise5 Reverse

---
+++ week-02/day-3/exercise5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reverse
+{
+    public class Reverse
+    {
+        public static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a function that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+
+            InOrder(reversed);
+            Console.Read();
+        }
+        public static void InOrder(string reversed) 
+        {
+            char[] inorder = reversed.ToCharArray();
+            Array.Reverse(inorder);
+            
+            Console.WriteLine(inorder);
+        }
+    }
+}
