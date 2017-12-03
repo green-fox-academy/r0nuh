160852731b7db5d8b18ddd4b4598d2c4a32f0f9a
r0nuh <cs.szilard@gmail.com>
2017-12-03 14:03:12 +0100

    feat(week2): add day2 swap array elements

---
+++ week-02/day-2/arrayExercise11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+
+            string[] abc = new string[] { "first", "second", "third" };
+
+            foreach (string element in abc)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+
+            string temp = abc[0];
+            abc[0] = abc[2];
+            abc[2] = temp;
+
+            foreach(string element in abc)
+            {
+            Console.Write(element + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
