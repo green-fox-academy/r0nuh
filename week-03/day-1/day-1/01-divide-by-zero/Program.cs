21210631d3207a96b527d36722b02da6b126ca90
r0nuh <cs.szilard@gmail.com>
2017-12-04 12:00:14 +0100

    feat(week3): add day1 exercise1 divide-by-zero

---
+++ week-03/day-1/day-1/01-divide-by-zero/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_divide_by_zero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // it should print "fail" if the parameter is 0
+
+            try
+            {
+            Divide10();
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("FAIL");
+            }
+            finally
+            {
+                Console.Read();
+            }
+
+
+        }
+        static void Divide10()
+        {
+            Console.Write("Divide 10 with: ");
+            int divisor = int.Parse(Console.ReadLine());
+
+            int x = 10;
+
+            Console.WriteLine("The result is: " + x / divisor);
+            Console.Read();
+        }
+    }
+}
