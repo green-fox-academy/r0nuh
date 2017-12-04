666e0bf070a424b91baf07e0ae1f1a8dba445c57
r0nuh <cs.szilard@gmail.com>
2017-12-04 16:04:23 +0100

    feat(week3): add day1 exercise4 write-single-line

---
+++ week-03/day-1/04-write-single-line/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace _04_write_single_line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open a file called "my-file.txt"
+            // Write your name in it as a single line
+            // If the program is unable to write the file,
+            // then it should print an error message like: "Unable to write file: my-file.txt"
+
+            string path = @"C:\Users\Szilard\Documents\greenfox\r0nuh\week-03\day-1\my-file.txt";
+
+            using (StreamWriter writer = new StreamWriter(path, true)) 
+            {
+                writer.WriteLine("Szilard Csiki");
+            }
+        }
+    }
+}
