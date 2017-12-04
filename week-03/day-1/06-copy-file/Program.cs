d5bfb369c4d6ef733cb3eb4c27f475af5b95ba14
r0nuh <cs.szilard@gmail.com>
2017-12-04 23:33:50 +0100

    feat(week3): add day1 exercise6 copy file

---
+++ week-03/day-1/06-copy-file/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace _06_copy_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+            // It should take the filenames as parameters
+            // It should return a boolean that shows if the copy was successful
+
+            string fileFrom = @"C:\Users\Szilard\Documents\greenfox\r0nuh\week-03\day-1\my-file.txt";
+            string fileTo = @"C:\Users\Szilard\Documents\greenfox\r0nuh\week-03\day-1\my-file-copy.txt";
+
+            //try 
+	        //{	        
+            Console.WriteLine(Copy(fileFrom, fileTo));
+            
+
+            Console.Read();
+	        //}
+	        //finally
+	        //{
+          
+        }
+        static bool Copy(string path1, string path2)
+        {
+            string content = File.ReadAllText(path1);
+
+            if(content != null) 
+            {
+            File.WriteAllText(path2, content);
+            return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+    }
+}
