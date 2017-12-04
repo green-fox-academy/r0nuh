96734ad78243aab1f8355a69a552e883090347da
r0nuh <cs.szilard@gmail.com>
2017-12-04 12:42:37 +0100

    feat(week3): add day1 exercise2 print-each-line

---
+++ week-03/day-1/02-print-each-line/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+
+namespace _02_print_each_line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that opens a file called "my-file.txt", then prints
+            // each of lines from the file.
+            // If the program is unable to read the file (for example it does not exists),
+            // then it should print an error message like: "Unable to read file: my-file.txt"
+
+            try
+            {
+            string content = File.ReadAllText(@"C:\Users\Szilard\Documents\greenfox\my-file.txt");
+                Console.WriteLine(content);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to read file: my-file.txt");
+            }
+            finally
+            {
+            Console.Read();
+            }
+        }
+    }
+}
