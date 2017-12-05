20e99e20bea69bb82e28ee9d7500e617b6587aa7
r0nuh <cs.szilard@gmail.com>
2017-12-05 22:52:21 +0100

    feat(week2): add day3 List exercise1

---
+++ week-02/day-3/listsExercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarSystem
+{
+    public class SolarSystem
+    {
+        public static void Main(string[] args)
+        { 
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+        
+
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+
+            planetList.Insert(5, "Saturn");
+
+            foreach (var planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }       
+	
+            Console.Read();
+
+	
+        }
+    }
+}
