e9d866b7ccab3f72b263f7728d9b550efb780216
r0nuh <cs.szilard@gmail.com>
2017-12-06 14:29:59 +0100

    feat(week3): add day3 05-horizontal

---
+++ week-03/day-3/05-horizontal/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace _05_horizontal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // create a line drawing function that takes 2 parameters:
+            // the x and y coordinates of the line's starting point
+            // and draws a 50 long horizontal line from that point.
+            // draw 3 lines with that function.
+
+            DrawLines(foxDraw, 0, 100);
+            DrawLines(foxDraw, 100, 10);
+            DrawLines(foxDraw, 50, 300);
+        }
+        public static void DrawLines(FoxDraw foxDraw, int x, int y)
+        {
+            foxDraw.BackgroundColor(Colors.LightGray);
+            var startPoint = new Point(x, y);
+            var endPoint = new Point(x + 50, y);
+            foxDraw.DrawLine(startPoint, endPoint);
+        }
+    }
+}
