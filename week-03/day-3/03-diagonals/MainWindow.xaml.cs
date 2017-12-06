f42d28beb1b5afcab33777db4b726880b9f74aa2
r0nuh <cs.szilard@gmail.com>
2017-12-06 13:50:13 +0100

    feat(week3): add day3 03-diagonals

---
+++ week-03/day-3/03-diagonals/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace _03_diagonals
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw the canvas' diagonals in green.
+
+            DrawLines(foxDraw);
+        }
+        public static void DrawLines(FoxDraw foxDraw)
+        {
+            foxDraw.BackgroundColor(Colors.LightGray);
+            foxDraw.StrokeColor(Colors.Green);
+
+            var startPoint = new Point(0, 0);
+            var endPoint = new Point(525, 350);
+            foxDraw.DrawLine(startPoint, endPoint);
+
+            var startPoint1 = new Point(0, 350);
+            var endPoint1 = new Point(525, 0);
+            foxDraw.DrawLine(startPoint1, endPoint1);
+        }
+    }
+}
