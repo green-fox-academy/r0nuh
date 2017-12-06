3ad4295eae56fb48b2179ddfc919186e7354e2be
r0nuh <cs.szilard@gmail.com>
2017-12-06 11:41:21 +0100

    feat(week3): add day3 01-middle-lines

---
+++ week-03/day-3/01-middle lines/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using GreenFox;
+using System.Windows.Media;
+
+namespace _01_middle_lines
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            //  draw a red horizontal line to the canvas' middle.
+            //  draw a green vertical line to the canvas' middle.
+
+            DrawLines(foxDraw);
+        }
+
+        public static void DrawLines(FoxDraw foxDraw)
+        {
+            foxDraw.BackgroundColor(Colors.Gray);
+            foxDraw.StrokeColor(Colors.Red); 
+            foxDraw.DrawLine(262.5, 0, 262.5, 350);
+
+            foxDraw.StrokeColor(Colors.Green);
+            var startPoint = new Point(0, 175);
+            var endPoint = new Point(525, 175);
+            foxDraw.DrawLine(startPoint, endPoint);
+        }
+    }
+}
