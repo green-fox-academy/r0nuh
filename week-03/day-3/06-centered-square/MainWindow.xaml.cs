f2c7ffa392edd6dcdaa31f9c8c966c63ac3f9a98
r0nuh <cs.szilard@gmail.com>
2017-12-06 14:47:12 +0100

    feat(week3): add day3 06-centered-square

---
+++ week-03/day-3/06-centered-square/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace _06_centered_square
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw a green 10x10 square to the canvas' center.
+
+            DrawRectangle(foxDraw);
+        }
+        public static void DrawRectangle(FoxDraw foxDraw)
+        {
+            foxDraw.BackgroundColor(Colors.LightSalmon);
+            foxDraw.DrawRectangle(257.5, 170, 10, 10);
+        }
+    }
+}
