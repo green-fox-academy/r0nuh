e90a5960716e49c611fbdc8b33180f0e6e493008
r0nuh <cs.szilard@gmail.com>
2017-12-06 16:16:03 +0100

    feat(week03): add day3 09-centered-boxes

---
+++ week-03/day-3/09-centered-boxes/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace _09_centered_boxes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // create a square drawing function that takes 1 parameter:
+            // the square size
+            // and draws a square of that size to the center of the canvas.
+            // draw 3 squares with that function.
+
+            DrawRectangle(foxDraw, 100);
+            DrawRectangle(foxDraw, 50);
+            DrawRectangle(foxDraw, 10);
+        }
+        static void DrawRectangle(FoxDraw foxDraw, int size)
+        {
+            foxDraw.DrawRectangle(263 - (size / 2), 175 - (size / 2), size, size);
+        }
+    }
+}
