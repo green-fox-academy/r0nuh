a7de73ff89305f16ee920374e84819925090ce11
r0nuh <cs.szilard@gmail.com>
2017-12-06 10:24:17 +0100

    feat(week2): add day3 List exercise3

---
+++ week-02/day-3/listsExercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppendLetter
+{
+    public class AppendLetter
+    {
+        public static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+
+            foreach(string animal in far)
+            {
+                Console.WriteLine(animal + "a");
+            }
+
+            Console.Read();
+        }
+    }
+}
