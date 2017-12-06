6b8b89d534524ce444e50095ec1f7843244f92c1
r0nuh <cs.szilard@gmail.com>
2017-12-06 15:38:21 +0100

    feat(week03): add day3 07-four-color-rectangles

---
+++ week-03/day-3/07-four-color-rectangles/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _07_four_color_rectangles
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ week-03/day-3/07-four-color-rectangles/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace _07_four_color_rectangles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw four different size and color rectangles.
+
+            DrawRectangles(foxDraw);
+        }
+        static void DrawRectangles(FoxDraw foxDraw)
+        {
+            foxDraw.FillColor(Colors.LightSalmon);
+            foxDraw.DrawRectangle(211, 124, 100, 100);
+
+            foxDraw.FillColor(Colors.Aquamarine);
+            foxDraw.DrawRectangle(162, 75, 50, 50);
+
+            foxDraw.FillColor(Colors.LemonChiffon);
+            foxDraw.DrawRectangle(310, 85, 30, 40);
+
+            foxDraw.FillColor(Colors.Red);
+            foxDraw.DrawRectangle(140, 270, 230, 40);
+        }
+    }
+}
