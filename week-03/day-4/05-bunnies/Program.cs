531c7890298d522f0bb562c402b3465eb1f70220
r0nuh <cs.szilard@gmail.com>
2017-12-07 18:21:03 +0100

    feat(week-03): add day4 05-bunnies

---
+++ week-03/day-4/05-bunnies/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05_bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+            Console.WriteLine(NumberOfEars(33, 2));
+            Console.Read();
+        }
+        static int NumberOfEars(int bunnies, int ears)
+        {
+            if (bunnies > 1)
+            {
+                bunnies--;
+                return NumberOfEars(bunnies, ears) + ears;
+            }
+            else
+            {
+                return ears;
+            }
+        }
+    }
+}
