0584a5fc13b567b43470047bb13279795ecb4b8e
r0nuh <cs.szilard@gmail.com>
2017-12-07 14:12:39 +0100

    feat(week03): add day4 02-numberadder

---
+++ week-03/day-4/02-numberadder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace _02_numberadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+            Console.WriteLine(NumberAdder(5));
+            Console.Read();
+        }
+        public static int NumberAdder(int n)
+        {
+            if (n > 0)
+            {
+                return n + NumberAdder(n - 1); 
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
