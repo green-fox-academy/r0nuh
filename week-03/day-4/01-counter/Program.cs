d0372e79d0cd9f430fd941d2cc36640ff0b7ef19
r0nuh <cs.szilard@gmail.com>
2017-12-07 13:48:21 +0100

    feat(week03): add day4 01-counter

---
+++ week-03/day-4/01-counter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and counts down from n.
+            CountDown(20);
+            Console.Read();
+        }
+        static void CountDown(int n)
+        {
+            if (n >= 0)
+            {
+                Console.WriteLine(n);
+                CountDown(n - 1);
+            }
+        }
+    }
+}
