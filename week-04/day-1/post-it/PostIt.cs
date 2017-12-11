1d0c0d1e21ae4eb556ff144e87cc75c1841fbe21
r0nuh <cs.szilard@gmail.com>
2017-12-11 12:21:24 +0100

    feat(week-04): day1 post-it

---
+++ week-04/day-1/post-it/PostIt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace post_it
+{
+    public struct PostIt
+    {
+        public string BackgroundColor;
+        public string Text;
+        public string TextColor;
+    }
+}
---
+++ week-04/day-1/post-it/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace post_it
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostIt postIt1 = new PostIt();
+            postIt1.BackgroundColor = "orange";
+            postIt1.Text = "Idea 1";
+            postIt1.TextColor = "blue";
+
+            PostIt postIt2 = new PostIt();
+            postIt2.BackgroundColor = "orange";
+            postIt2.Text = "Awesome";
+            postIt2.TextColor = "blue";
+
+            PostIt postIt3 = new PostIt();
+            postIt3.BackgroundColor = "orange";
+            postIt3.Text = "Superb!";
+            postIt3.TextColor = "blue";
+        }
+    }
+}
