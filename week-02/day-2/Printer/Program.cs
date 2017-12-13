4bc4f82d8e7fb8549a610919b9e5a03054e563b7
r0nuh <cs.szilard@gmail.com>
2017-12-13 23:42:15 +0100

    feat(week2): day-2 Printer

---
+++ week-02/day-2/Printer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+
+            Printer("first", "second", "third");
+            Console.Read();
+        }
+        static void Printer(params string[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+
+            Console.WriteLine(input[i]);
+            }
+        }
+    }
+}
