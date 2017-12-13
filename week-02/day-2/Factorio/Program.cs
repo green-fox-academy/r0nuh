d98e42c87f72891eceac3bd6ae97a7c75328bffd
r0nuh <cs.szilard@gmail.com>
2017-12-13 17:09:46 +0100

    feat(week-02): day2 Factorio

---
+++ week-02/day-2/Factorio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Factorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+
+            Console.WriteLine(Factorio(5));
+            Console.ReadLine();
+        }
+
+        static int Factorio(int x)
+        {
+            int fact = 1;
+            if (x > 0)
+            {
+                for (int i = 1; i <= x; i++)
+                {
+                    fact *= i;
+                }
+                return fact;
+            }
+            else
+            {
+                return fact;
+            }
+        }
+    }
+}
