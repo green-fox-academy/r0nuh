d45434c221f8cf7e388c7a504e622fc516776b4c
r0nuh <cs.szilard@gmail.com>
2017-12-13 13:52:53 +0100

    feat(week-02): day1 exercise28

---
+++ week-02/day-1/exercise28/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+
+            Console.Write("Give a number: ");
+            int lines = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < lines; ++i)
+            {
+                for ( int j = 0; j <= i; ++j)
+                {
+                Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
