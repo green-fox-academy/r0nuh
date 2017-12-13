2429adc6e3ab5e68469f7d2e6e8c2ec7e7b8e248
r0nuh <cs.szilard@gmail.com>
2017-12-14 00:19:15 +0100

    feat(week2): day-2 SumAll

---
+++ week-02/day-2/SumAll/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `ai`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Print the sum of the elements in `ai`
+
+            int[] ai = { 3, 4, 5, 6, 7 };
+            int sum = 0;
+
+            foreach (int number in ai)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
