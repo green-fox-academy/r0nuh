08ac505662db3f0c4b3e8ab63f3f450a3187ea03
r0nuh <cs.szilard@gmail.com>
2017-12-14 07:43:28 +0100

    feat(week2): day-2 Reverse

---
+++ week-02/day-2/Reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+
+            int[] aj = { 3, 4, 5, 6, 7 };
+            Array.Reverse(aj);
+            PrintElements(aj);
+
+            Console.ReadLine();
+        }
+        static void PrintElements(Array array)
+        {
+            foreach (int number in array)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
