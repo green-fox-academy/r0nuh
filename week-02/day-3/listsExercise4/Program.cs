33eebd73b0ab1fdde6ff79b81de5b34904975aaf
r0nuh <cs.szilard@gmail.com>
2017-12-16 22:23:46 +0100

    feat(week2): add day3 List exercise4

---
+++ week-02/day-3/listsExercise4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CandyShop
+{
+    public class Candyshop
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            // Accidentally we added "2" and "false" to the list. 
+            // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+            // No, don't just remove the lines
+
+            /*
+             * // First solution:
+            list[1] = "Croissant";
+            list[3] = "Ice cream";
+            */
+
+            list.Insert(list.IndexOf(2),"Croissant");
+            list.Remove(2);
+
+            list.Insert(list.IndexOf(false), "Ice cream");
+            list.Remove(false);
+
+            foreach (var item in list)
+            {
+            Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+    }
+}
