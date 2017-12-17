d75fb87b650a8e7434f4b24aa2037ba2d537a84b
r0nuh <cs.szilard@gmail.com>
2017-12-17 11:54:34 +0100

    feat(week2): add day3 List exercise5

---
+++ week-02/day-3/listsExercise5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementFinder
+{
+    public class ElementFinder
+    {
+        public static void Main(string[] args)
+        {
+            var List = new List<int> { 1, 2, 3, 4, 5, 7 };
+
+            // Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+            // Do this again with a different solution using different list functions!
+
+            CheckForSeven(List);
+            CheckFor7(List);
+            CheckSeven(List);
+
+            Console.Read();
+        }
+
+        static void CheckForSeven(List<int> list)
+        {
+            if (list.Contains(7))
+                Console.WriteLine("Hoorray");
+            else
+                Console.WriteLine("Noooooo");
+        }
+
+        static void CheckFor7(List<int> list)
+        {
+            if (list.Exists(element => element == 7))
+                Console.WriteLine("Hoorray");
+            else
+                Console.WriteLine("Noooooo");
+        }
+
+        static void CheckSeven(List<int> list)
+        {
+            int seven = list.Find(element => element == 7);
+
+            if (seven == 7)
+                Console.WriteLine("Hoorray");
+            else
+                Console.WriteLine("Noooooo");
+        }
+    }
+}
