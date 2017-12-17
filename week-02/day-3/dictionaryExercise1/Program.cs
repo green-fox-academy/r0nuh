08fb27df713bb43813b91589378c5e807ac73ba5
r0nuh <cs.szilard@gmail.com>
2017-12-17 18:55:54 +0100

    feat(week2): add day3 Dictionary exercise1

---
+++ week-02/day-3/dictionaryExercise1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeWillNever
+{
+    public class HeWillNever
+    {
+        public static void Main(string[] args)
+        {
+            //var output = new StringBuilder();
+            string output = " ";
+            int[] notSoCrypticMessage = { 1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11 };
+
+            var map = new Dictionary<int, string>();
+
+            map.Add(7, "run around and desert you");
+            map.Add(50, "tell a lie and hurt you");
+            map.Add(49, "make you cry,");
+            map.Add(2, "let you down");
+            map.Add(12, "give you up,");
+            map.Add(1, "Never gonna");
+            map.Add(11, "\n");
+            map.Add(3, "say goodbye");
+
+            // Things are a little bit messed up
+            // Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
+            // Assemble the fragments into the out variable
+
+            for (int i = 0; i < notSoCrypticMessage.Length; i++)
+            {
+                //output.Append(map[notSoCrypticMessage[i]] + " ");
+                output += map[notSoCrypticMessage[i]] + " ";
+            }
+
+            Console.Write(output);
+            Console.Read();
+        }
+    }
+}
