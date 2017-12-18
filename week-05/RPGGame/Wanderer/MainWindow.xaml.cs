703466849bfce21d6e3b5c0f27dcce89f95831a1
r0nuh <cs.szilard@gmail.com>
2017-12-18 19:42:29 +0100

    feat(RPGGame): draw map with walls

---
+++ week-05/RPGGame/Wanderer/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using GreenFox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wanderer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var FoxDraw = new FoxDraw(canvas);
+
+            int[,] map = new int[,]
+            {
+                {0, 0, 0, 1, 0, 1, 0, 0, 0, 0 },
+                {0, 0, 0, 1, 0, 1, 0, 1, 1, 0 },
+                {0, 1, 1, 1, 0, 1, 0, 1, 1, 0 },
+                {0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+                {1, 1, 1, 1, 0, 1, 1, 1, 1, 0 },
+                {0, 1, 0, 1, 0, 0, 0, 0, 1, 0 },
+                {0, 1, 0, 1, 0, 1, 1, 0, 1, 0 },
+                {0, 0, 0, 0, 0, 1, 1, 0, 1, 0 },
+                {0, 1, 1, 1, 0, 0, 0, 0, 1, 0 },
+                {0, 0, 0, 1, 0, 1, 1, 0, 1, 0 },
+                {0, 1, 0, 1, 0, 1, 0, 0, 0, 0 }
+            };
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (map[i,j] == 0)
+                    {
+                        FoxDraw.AddImage("Assets/floor.png", x, y);
+                        x += 50;
+                    }
+                    else
+                    {
+                        FoxDraw.AddImage("Assets/wall.png", x, y);
+                        x += 50;
+                    }
+                }
+                x = 0;
+                y += 50;
+            }
+        }
+
+     
+    }
+}
+
