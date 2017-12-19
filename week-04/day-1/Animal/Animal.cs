008b5b3990cdc85e5bd05ba48f0749271e0bec18
r0nuh <cs.szilard@gmail.com>
2017-12-19 14:53:02 +0100

    feat(week4): add day1 Animal

---
+++ week-04/day-1/Animal/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Animal
+{
+    class Animal
+    {
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+            Hunger = 50;
+            Thirst = 50;
+        }
+
+        public void Eat()
+        {
+            Hunger--;
+        }
+
+        public void Drink()
+        {
+            Thirst--;
+        }
+
+        public void Play()
+        {
+            Hunger++;
+            Thirst++;
+        }
+    }
+}
---
+++ week-04/day-1/Animal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal("cat");
+
+            Print(cat);
+
+            cat.Eat();
+            cat.Eat();
+            cat.Eat();
+            cat.Eat();
+            cat.Play();
+
+            Print(cat);
+
+            Console.Read();
+        }
+
+        static void Print(Animal animal)
+        {
+            Console.WriteLine("{0}'s hunger:{1}", animal.Name, animal.Hunger);
+            Console.WriteLine("{0}'s hunger:{1}", animal.Name, animal.Thirst);
+            Console.WriteLine();
+        }
+    }
+}
