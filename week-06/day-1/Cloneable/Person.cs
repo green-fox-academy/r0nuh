b4664b9a3b0e2bac1418313b70c578beaac1dea4
r0nuh <cs.szilard@gmail.com>
2018-01-02 15:55:55 +0100

    feat(week6): :mag: add day1 Cloneable

---
+++ week-06/day-1/Cloneable/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace green_fox
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age  {get; set; }
+        public string Gender { get; set; }
+
+        public Person(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public Person()
+        {
+            Name = "Jane Doe";
+            Age = 30;
+            Gender = "female";
+        }
+
+        public virtual void Introduce()
+        {
+            Console.WriteLine("Hi, I'm {0}, a {1} year old {2}", Name, Age, Gender);
+        }
+
+        public virtual void GetGoal()
+        {
+            Console.WriteLine("My goal is: Live for the moment!");
+        }
+    }
+}
---
+++ week-06/day-1/Cloneable/Program.cs
@@ -0,0 +1,27 @@
+﻿using green_fox;
+using System;
+using System.Collections.Generic;
+
+namespace Cloneable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            Student john = new Student("John Doe", 20, "male", "BME");
+            people.Add(john);
+
+            Student johnTheClone = (Student)john.Clone();
+            people.Add(johnTheClone);
+
+            foreach (Person person in people)
+            {
+                person.Introduce();
+            }
+
+            Console.Read();
+        }
+    }
+}
