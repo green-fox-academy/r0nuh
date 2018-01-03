aff96633a097b2e6807e2e728449edb497b95da2
r0nuh <cs.szilard@gmail.com>
2018-01-03 16:55:11 +0100

    feat(week6): :mag: add day2 exercise5 - Frequency of numbers

---
+++ week-06/day-2/FrequencyOfNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace FrequencyOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var frequencyOfNumbers = from number in n
+                                     group number by number;
+
+            foreach (var group in frequencyOfNumbers)
+            {
+                Console.WriteLine($"The number of {group.Key}'s in the array is: {group.Count()}");
+                //foreach (var item in group)
+                //{
+                //    Console.Write("  " + item);
+                //}
+                //Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            var frequencyOfNumbersWithLambda = n.GroupBy(i => i).ToDictionary(i => i.Key, i => i.Count());
+
+            foreach (var item in frequencyOfNumbersWithLambda)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+            Console.Read();
+        }
+    }
+}
