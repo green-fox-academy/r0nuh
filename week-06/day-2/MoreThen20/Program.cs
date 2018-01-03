fe6afdbb02d1e683d5548f66f3ac76e5edc01da2
r0nuh <cs.szilard@gmail.com>
2018-01-03 15:23:47 +0100

    feat(week6): :mag: add day2 exercise4 - Gives the numbers which squared value is more then 20

---
+++ week-06/day-2/MoreThen20/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoreThen20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var squaredNumbers = from numbers in n
+                                 where (numbers * numbers) > 20
+                                 select numbers;
+
+            foreach (var number in squaredNumbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+
+            IEnumerable<int> squaredNumbersWithLambda = n.Where(i => (i * i) > 20);
+
+            foreach (var num in squaredNumbersWithLambda)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.Read();
+        }
+    }
+}
