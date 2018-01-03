6059251421a9c757fe68fd66dc1411f75a8242eb
r0nuh <cs.szilard@gmail.com>
2018-01-03 17:20:36 +0100

    feat(week6): :mag: add day2 exercise6 - Frequency of characters in a string

---
+++ week-06/day-2/FrequencyOfCharacters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace FrequencyOfCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenString = "Exercise";
+
+            var frequencyOfCharacters = from character in givenString
+                                     group character by character;
+
+            foreach (var group in frequencyOfCharacters)
+            {
+                Console.WriteLine($"The number of {group.Key}'s in the string is: {group.Count()}");
+            }
+            Console.WriteLine();
+
+            var frequencyOfCharactersWithLambda = givenString.GroupBy(s => s).ToDictionary(s => s.Key, s => s.Count());
+
+            foreach (var item in frequencyOfCharactersWithLambda)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+            Console.Read();
+        }
+    }
+}
