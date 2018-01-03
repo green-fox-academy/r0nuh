b6382524db8a9b9d44d8c889165f6a275e3824ff
r0nuh <cs.szilard@gmail.com>
2018-01-04 00:29:59 +0100

    feat(week6): :mag: add day2 exercise8 - Uppercase characters in a string

---
+++ week-06/day-2/UppercaseChars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace UppercaseChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenString = "ExeRcise";
+
+            var uppercaseCharacters = from character in givenString
+                                      where character.ToString() == character.ToString().ToUpper()
+                                      select character;
+
+            foreach (var character in uppercaseCharacters)
+            {
+                Console.WriteLine(character);
+            }
+            Console.WriteLine();
+
+            var uppercaseCharactersWithLambda = givenString.Where(c => c.ToString().Equals(c.ToString().ToUpper()));
+
+            foreach (var character in uppercaseCharactersWithLambda)
+            {
+                Console.Write(character + " ");
+            }
+
+            Console.Read();
+        }
+    }
+}
