155702467967a3c39c09d3d8e6ef21add6175ed5
r0nuh <cs.szilard@gmail.com>
2018-01-03 17:38:57 +0100

    feat(week6): :mag: add day2 exercise7 - Cities starting with A ending with I

---
+++ week-06/day-2/Cities/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var citiesWithAI = from city in cities
+                               where city.StartsWith("A") && city.EndsWith("I")
+                               select city;
+
+            foreach (var city in citiesWithAI)
+            {
+                Console.WriteLine(city);
+            }
+
+            var citiesWithAIWithLambda = cities.Where(city => city.StartsWith("A") && city.EndsWith("I"));
+
+            foreach (var city in citiesWithAIWithLambda)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.Read();
+        }
+    }
+}
