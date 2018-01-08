653228f9db128cb87fc5fe876d490cb33e9f3cf8
r0nuh <cs.szilard@gmail.com>
2018-01-08 16:38:50 +0100

    feat(WebApplication1): count the greet number and show in id

---
+++ week-07/day-1/WebApplication1/Controllers/RESTController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        private static int idCounter = 0;
+
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
+        {
+            ++idCounter;
+            var greet = new Greeting()
+            {
+                Id = idCounter,
+                Content = name
+            };
+            return new JsonResult(greet);
+        }
+    }
+}
