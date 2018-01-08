382ca0acf8fa895b58a4f39c8be6e1b3127e60d9
r0nuh <cs.szilard@gmail.com>
2018-01-08 17:15:31 +0100

    feat(WebApplication1): write greet and site loading count

---
+++ week-07/day-1/WebApplication1/Controllers/WebController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("web")]
+    public class WebController : Controller
+    {
+        private static int idCounter = 0;
+
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
+        {
+            ++idCounter;
+            var greeting = new Greeting()
+            {
+                Id = idCounter,
+                Content = name
+            };
+
+            return View(greeting);
+        }
+
+    }
+}
