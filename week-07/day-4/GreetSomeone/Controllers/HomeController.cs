138ebb9e7a9770aabc71cc94ac74e817f1b77b8d
r0nuh <cs.szilard@gmail.com>
2018-01-11 17:42:45 +0100

    feat(day-4): add GreetSomeone with input form

---
+++ week-07/day-4/GreetSomeone/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GreetSomeone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GreetSomeone.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private User name;
+
+        public HomeController(User name)
+        {
+            this.name = name;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("")]
+        public IActionResult Submit()
+        {
+            return RedirectToAction("Greet");
+        }
+
+        [HttpGet("greet")]
+        public IActionResult Greet()
+        {
+            return Ok();
+        }
+    }
+}
