98edc23efb6a783e110a026e999e9459986b573b
r0nuh <cs.szilard@gmail.com>
2018-01-26 00:04:24 +0100

    feat(ListingTodos): login page

---
+++ week-08/day-1/ListingTodos/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ListingTodos.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ListingTodos.Controllers
+{
+    [Route("")]
+    public class UserController : Controller
+    {
+        private TodoRepository todoRepository;
+
+        public UserController(TodoRepository todoRepository)
+        {
+            this.todoRepository = todoRepository;
+        }
+
+        [HttpGet("")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
